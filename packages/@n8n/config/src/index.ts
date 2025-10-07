3847274e759c28556f63a874e43a9c4579e61ace
= <=>
2025-10-07 18:53:49 -0300

    Host IP da máquina

--- packages/@n8n/config/src/index.ts
+++ packages/@n8n/config/src/index.ts
@@ -102,7 +102,7 @@ export class GlobalConfig {
 
 	/** Host name n8n can be reached */
 	@Env('N8N_HOST')
-	host: string = 'localhost';
+	host: string = '10.14.131.99';
 
 	/** HTTP port n8n can be reached */
 	@Env('N8N_PORT')
